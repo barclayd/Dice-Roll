5229e93a68f9c2ca8b0948787effff43721116b2
Daniel Barclay <barclaysd@me.com>
2019-05-31 23:45:33 +0100

    Basic functionality to randomise dice face upon launch and when roll button is pressed

--- Dice Roll/ViewController.swift
+++ Dice Roll/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateDice()
     }
+    
+    let diceOptionsArray : Array = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDice()
+    }
+    
+    func updateDice() {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage.init(named: diceOptionsArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage.init(named: diceOptionsArray[randomDiceIndex2])
+    }
+    
 }
 
